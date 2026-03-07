34c981a9820cc10aec1d81340cedb5faf221738f
amirdjalali <amirdjalali@unibo.it>
2026-03-07 09:25:32 +0100

    edited upload_handler class and subclasses

---
+++ handlers/bibliographic_entity_upload_handler.py
@@ -0,0 +1,11 @@
+from .upload_handler import UploadHandler
+
+class BibliographicEntityUploadHandler(UploadHandler):
+    """
+    This class implements the method of the superclass to handle the specific scenario
+    BibliographicEntityUploadHandler handles JSON files in input and to store their data in a relational database.
+    """
+
+    pass
+
+    # no __init__ method needed if no additional attributes are added
---
+++ handlers/upload_handler.py
@@ -0,0 +1,17 @@
+from .handler import Handler
+
+class UploadHandler(Handler):
+    """
+    Methods
+    -------
+    pushDataToDb(path)
+        it takes in input the path of a file containing annotations 
+        and uploads them in the database. This method can be called everytime 
+        there is a need to upload annotations in the database. 
+        The actual implementation of this method is left to its subclasses.
+    """
+
+    # no __init__ method needed if no additional attributes are added
+
+    def PushDataToDbPath(self, path: str) -> bool:
+        pass
