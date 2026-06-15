c82c801d9b5c59bf76f3267084880863844b1bca
Johanna Marthe <johanna.marthe.israel@gmail.com>
2026-06-15 18:26:06 +0200

    fixing names

--- handlers/bibliographic_entity_query_handler.py
+++ handlers/bibliographic_entity_query_handler.py
@@ -4,13 +4,11 @@ from .query_handler import QueryHandler
 
 class BibliographicEntityQueryHandler(QueryHandler):
 
-# -- getting all metadata except for author and id (will come later in the basic query engine??)
-
     def getAllBibliographicEntities(self) -> pd.DataFrame: # creates a dataframe out of all the core metadata -> has no filters; this line defines the method, only self as input argument; output will be a pandas dataframe
         query = """  
-            SELECT internal_id, title, pub_date, venue 
-            FROM df_entities
-        """ # stores multi-line SQL statement inside a text variable, query. commands the database to check the main metadata table df_entities and fetches every single record, using the four required columns. 
+            SELECT internalId, title, publication_date, venue 
+            FROM BibliographicEntity
+        """ # stores multi-line SQL statement inside a text variable, query. commands the database to check the main metadata table BibliographicEntity and fetches every single record, using the four required columns. 
         with sqlite3.connect(self.dbPathOrUrl) as con: # opens pipe connection (con) to the local SQLite database file path -> placeholder will update automatically depending on the test file 
             return pd.read_sql_query(query, con) # pandas takes the query text and the database pipe con, collects the resulting rows using SQLite, converting them to a clean, labelled dataframe table 
 
@@ -18,8 +16,8 @@ class BibliographicEntityQueryHandler(QueryHandler):
 
     def getBibliographicEntitiesWithTitle(self, title: str) -> pd.DataFrame:
         query = """
-            SELECT internal_id, title, pub_date, venue
-            FROM df_entities
+            SELECT internalId, title, publication_date, venue
+            FROM BibliographicEntity
             WHERE title LIKE ?
         """
         with sqlite3.connect(self.dbPathOrUrl) as con:
@@ -29,8 +27,8 @@ class BibliographicEntityQueryHandler(QueryHandler):
 
     def getBibliographicEntitiesWithVenue(self, venue: str) -> pd.DataFrame:
         query = """
-            SELECT internal_id, title, pub_date, venue
-            FROM df_entities
+            SELECT internalId, title, publication_date, venue
+            FROM BibliographicEntity
             WHERE venue LIKE ?
         """
         with sqlite3.connect(self.dbPathOrUrl) as con:
@@ -40,8 +38,8 @@ class BibliographicEntityQueryHandler(QueryHandler):
 
     def getBibliographicEntitiesWithinPublicationDate(self, start_date: str = None, end_date: str = None) -> pd.DataFrame: # assigning None will make them optional, in case one of the boundaries is left empty?
         query = """
-            SELECT internal_id, title, pub_date, venue
-            FROM df_entities
+            SELECT internalId, title, publication_date, venue
+            FROM BibliographicEntity
             WHERE 1=1
         """ # 1=1 is a trick to build dynamic queries without knowing what a user's filter will look like. -> placeholder that is always true
         
@@ -50,12 +48,12 @@ class BibliographicEntityQueryHandler(QueryHandler):
 
         # checks if a starting date limit was provided by the test file
         if start_date: # might appear in the test file
-            query += " AND pub_date >= ?" # optional filters -> query is updated to include the start date
+            query += " AND publication_date >= ?" # optional filters -> query is updated to include the start date
             date_list.append(start_date)
 
         # checks if a end date limit was provided by the test file
         if end_date: # might appear in the test file
-            query += " AND pub_date <= ?" # optional filters -> query is updated to also include the end date 
+            query += " AND publication_date <= ?" # optional filters -> query is updated to also include the end date 
             date_list.append(end_date)
 
         with sqlite3.connect(self.dbPathOrUrl) as con:
@@ -68,10 +66,10 @@ class BibliographicEntityQueryHandler(QueryHandler):
         # even if an entity matches multiple criteria during the filtering process
         # JOIN temporarily matches main entities table with author table
         query = """
-            SELECT DISTINCT df_entities.internal_id, df_entities.title, df_entities.pub_date, df_entities.venue 
-            FROM df_entities
-            JOIN df_authors ON df_entities.internal_id = df_authors.internal_id
-            WHERE df_authors.author_name LIKE ?
+            SELECT DISTINCT BibliographicEntity.internalId, BibliographicEntity.title, BibliographicEntity.publication_date, BibliographicEntity.venue 
+            FROM BibliographicEntity
+            JOIN BibliographicEntityAuthor ON BibliographicEntity.internalId = BibliographicEntityAuthor.entityId
+            WHERE BibliographicEntityAuthor.author LIKE ?
         """
         # opens a temporary, secure connection bridge to the underlying SQLite database file
         with sqlite3.connect(self.dbPathOrUrl) as con:
@@ -85,10 +83,10 @@ class BibliographicEntityQueryHandler(QueryHandler):
         # strict equals sign (=) instead of LIKE because unique identifier lookups must be exact
         # SELECT line only requests four core columns, dropping the ID string itself
         query = """
-            SELECT df_entities.internal_id, df_entities.title, df_entities.pub_date, df_entities.venue 
-            FROM df_entities
-            JOIN df_ids ON df_entities.internal_id = df_ids.internal_id
-            WHERE df_ids.identifier_value = ?
+            SELECT BibliographicEntity.internalId, BibliographicEntity.title, BibliographicEntity.publication_date, BibliographicEntity.venue 
+            FROM BibliographicEntity
+            JOIN BibliographicEntityId ON BibliographicEntity.internalId = BibliographicEntityId.entityID
+            WHERE BibliographicEntityId.id = ?
         """
 
         with sqlite3.connect(self.dbPathOrUrl) as con:
