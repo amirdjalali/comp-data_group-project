9330620b5aa7698bfac25f2d51de4b52c4569925
amirdjalali <amir.djalali@unibo.it>
2026-06-28 15:48:31 +0200

    tests with upload handlers, to be removed

--- handlers/bibliographic_entity_query_handler.py
+++ handlers/bibliographic_entity_query_handler.py
@@ -1,22 +1,136 @@
 from .query_handler import QueryHandler
-import pandas as pd
+from sqlite3 import connect
+from pandas import DataFrame, Series, read_sql
 
 class BibliographicEntityQueryHandler(QueryHandler):
 
-    def getById(self, id: str) -> pd.DataFrame:
-        return pd.DataFrame()
+    def getById(self, id: str) -> DataFrame:
 
-    def getAllBibliographicEntities(self) -> pd.DataFrame:
-        return pd.DataFrame()
+        with connect(self.getDbPathOrUrl()) as con:
+            query_id = f"""
+                SELECT *
+                FROM BibliographicEntity
+                JOIN BibliographicEntityIDs ON BibliographicEntity.bib_item_id = BibliographicEntityIDs.bibitem_id
+                WHERE id = "{id}"
+            """
+            df_sql = read_sql(query_id, con)
+            bib_item_id = df_sql.at[0, "bib_item_id"]
 
-    def getBibliographicEntitiesWithTitle(self, title: str) -> pd.DataFrame:
-        return pd.DataFrame()
+            print(df_sql)
+            print(bib_item_id)
 
-    def getBibliographicEntitiesWithAuthor(self, author: str) -> pd.DataFrame:
-        return pd.DataFrame()
+            query_ids = f"""
+                SELECT *
+                FROM BibliographicEntityIDs
+                WHERE bibitem_id = "{bib_item_id}"
+            """
 
-    def getBibliographicEntitiesWithinPublicationDate(self, start_date: str, end_date: str) -> pd.DataFrame:
-        return pd.DataFrame()
+            df_ids = read_sql(query_ids, con)
+            ids = []
+            for i, row in df_ids.iterrows():
+                ids.append(row["id"])
+            
+            df_sql["id"] = df_sql["id"].astype('object')
+            df_sql.at[0, "id"] = ids
 
-    def getBibliographicEntitiesWithVenue(self, venue: str) -> pd.DataFrame:
-        return pd.DataFrame()
+            query_authors = f"""
+                SELECT name, pos
+                FROM BibliographicEntityAuthors
+                JOIN Author ON BibliographicEntityAuthors.author_id = Author.author_id
+                WHERE bibitem_id = "{bib_item_id}"
+            """
+
+            df_authors = read_sql(query_authors, con)
+            authors = []
+            for i, row in df_authors.iterrows():
+                authors.append((row["name"], row["pos"]))
+            authors = sorted(authors, key=lambda x: x[1])
+            authors_list = []
+            for author, pos in authors:
+                authors_list.append(author)
+            df_sql["authors"] = Series(dtype="object")
+            df_sql.at[0, "authors"] = authors_list
+            
+            print(df_sql)
+
+        return df_sql
+
+    def getAllBibliographicEntities(self) -> DataFrame:
+
+        with connect(self.getDbPathOrUrl()) as con:
+            query_id = f"""
+                SELECT *
+                FROM BibliographicEntity
+                JOIN BibliographicEntityIDs ON BibliographicEntity.bib_item_id = BibliographicEntityIDs.bibitem_id
+            """
+            df_id = read_sql(query_id, con)
+            print(df_id)
+
+            ids = {}
+
+            for idx, row in df_id.iterrows():
+                if row["bibitem_id"] not in ids:
+                    ids[row["bibitem_id"]] = []
+                ids[row["bibitem_id"]].append(row["id"])
+            
+            df_ids = DataFrame(ids.items(), columns=["bibitem_id", "ids"])
+            print(df_ids)
+
+            query_bibentity = f"""
+                SELECT *
+                FROM BibliographicEntity
+            """
+            df_entities = read_sql(query_bibentity, con)
+            print(df_entities)
+            new_df = df_entities.merge(df_ids, left_on="bib_item_id", right_on="bibitem_id").drop(columns="bibitem_id")
+
+            print(new_df)
+
+            query_authors = """
+                SELECT *
+                FROM BibliographicEntityAuthors
+                JOIN Author ON BibliographicEntityAuthors.author_id = Author.author_id
+            """
+            df_authors = read_sql(query_authors, con)
+            print(df_authors)
+
+            authors = {}
+
+            for idx, row in df_authors.iterrows():
+                if row["bibitem_id"] not in authors:
+                    authors[row["bibitem_id"]] = []
+                authors[row["bibitem_id"]].append((row["name"], row["pos"]))
+            
+            for key in authors:
+                ordered_pos = sorted(authors[key], key=lambda x: x[1])
+                ordered = []
+                for author, pos in ordered_pos:
+                    ordered.append(author)
+                authors[key] = ordered
+            
+            df_autori = DataFrame(authors.items(), columns=["bibitem_id", "authors"])
+            print(df_autori)
+
+            final_df = new_df.merge(df_autori, left_on="bib_item_id", right_on="bibitem_id").drop(columns="bib_item_id")
+
+            print(final_df)
+
+        return final_df
+
+    def getBibliographicEntitiesWithTitle(self, title: str) -> DataFrame:
+        return DataFrame()
+
+    def getBibliographicEntitiesWithAuthor(self, author: str) -> DataFrame:
+        return DataFrame()
+
+    def getBibliographicEntitiesWithinPublicationDate(self, start_date: str, end_date: str) -> DataFrame:
+        return DataFrame()
+
+    def getBibliographicEntitiesWithVenue(self, venue: str) -> DataFrame:
+        return DataFrame()
+
+if __name__ == "__main__":
+    handler = BibliographicEntityQueryHandler()
+    handler.setDbPathOrUrl("relational.db")
+    #handler.getById("openalex:W3193297707")
+    handler.getAllBibliographicEntities()
--- handlers/bibliographic_entity_upload_handler.py
+++ handlers/bibliographic_entity_upload_handler.py
@@ -4,16 +4,54 @@ from .upload_handler import UploadHandler
 
 class BibliographicEntityUploadHandler(UploadHandler):
     def pushDataToDb(self, path: str) -> bool:
-        bibdf = pd.read_json(path)
-        for i, row in bibdf.iterrows():
-            if isinstance(row['id'], list):
-                bibdf.at[i, 'id'] = '; '.join(row['id'])
-            if isinstance(row['author'], list):
-                bibdf.at[i, 'author'] = '; '.join(row['author'])
-       
-        with sqlite3.connect(self.dbPathOrUrl) as con:
-            bibdf.to_sql('BibliographicEntity', con, if_exists='replace', index=False)
+        general_df = pd.read_json(path)
 
+        bibitem_rows = []
+        author_rows = []
+        authors_in_bibitem_rows = []
+        ids_in_bibitem_rows = []
+
+        #print(bibdf)
+        author_set = set()
+        author_counter = 0
+        number_of_bibitems = len(general_df)
+
+        for i, row in general_df.iterrows():
+            print(f"Parsing bibliographic entity {i} of {number_of_bibitems}", end="\r")
+            bib_item_id = f"bibitem_{i}"
+            bibitem_rows.append([bib_item_id, row["title"], row["pub_date"], row["venue"]])
+            for pos, author in enumerate(row['author']):
+                author_counter += 1
+                author_id = f"author_{author_counter}"
+                # does not check for dupes
+                author_rows.append([author_id, author])
+                authors_in_bibitem_rows.append([bib_item_id, author_id, pos])
+            for id in row["id"]:
+                ids_in_bibitem_rows.append([bib_item_id, id])
+
+        bibitem_df = pd.DataFrame(bibitem_rows, columns=["bib_item_id", "title", "pub_date", "venue"])
+        author_df = pd.DataFrame(author_rows, columns=["author_id", "name"])
+        authors_in_bibitem_df = pd.DataFrame(authors_in_bibitem_rows, columns=["bibitem_id", "author_id", "pos"])
+        ids_in_bibitem_df = pd.DataFrame(ids_in_bibitem_rows, columns=["bibitem_id", "id"])
+
+        print("Writing data into db")
+
+        print(bibitem_df)
+        #print(author_df)
+        #print(authors_in_bibitem_df)
+        #print(ids_in_bibitem_df)
+            
+        with sqlite3.connect(self.dbPathOrUrl) as con:
+            bibitem_df.to_sql('BibliographicEntity', con, if_exists='replace', index=False)
+            author_df.to_sql('Author', con, if_exists='replace', index=False)
+            authors_in_bibitem_df.to_sql('BibliographicEntityAuthors', con, if_exists='replace', index=False)
+            ids_in_bibitem_df.to_sql('BibliographicEntityIDs', con, if_exists='replace', index=False)
+
+        print(f"Done! DB written in {self.dbPathOrUrl}")
         return True
 
+if __name__ == "__main__":
+    handler = BibliographicEntityUploadHandler()
+    handler.setDbPathOrUrl("relational.db")
+    handler.pushDataToDb("data/dh_metadata.json")
        
