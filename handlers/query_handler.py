f897b546383a509462a275f46fc59fe4976d75d8
Daniele Bottaro <daniele.bottaro@studio.unibo.it>
2026-07-13 17:19:23 +0200

    forgot abc class

--- handlers/query_handler.py
+++ handlers/query_handler.py
@@ -2,7 +2,7 @@ from .handler import Handler
 import pandas as pd
 from abc import ABC, abstractmethod
 
-class QueryHandler(Handler):
+class QueryHandler(Handler, ABC):
 
     @abstractmethod
     def getById(self, id: str) -> pd.DataFrame:
