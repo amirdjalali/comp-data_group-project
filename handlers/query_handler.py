f6adbec744509545dd24c8332f17451288bb5d34
Daniele Bottaro <daniele.bottaro@studio.unibo.it>
2026-07-13 17:17:43 +0200

    getbyid abstract method

--- handlers/query_handler.py
+++ handlers/query_handler.py
@@ -1,7 +1,10 @@
 from .handler import Handler
 import pandas as pd
+from abc import ABC, abstractmethod
 
 class QueryHandler(Handler):
 
+    @abstractmethod
     def getById(self, id: str) -> pd.DataFrame:
-        return pd.DataFrame()
+
+        pass
